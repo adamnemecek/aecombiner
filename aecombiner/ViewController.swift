c9ec6e6708e7ab41e3accb3e0a4bdaed79cc373a
djmlewis <djmlewis@mac.com>
2015-06-25 11:14:30 +0100

    Initial Commit

---
+++ aecombiner/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aecombiner
+//
+//  Created by David JM Lewis on 25/06/2015.
+//  Copyright (c) 2015 djml.eu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ aecombinerTests/aecombinerTests.swift
@@ -0,0 +1,36 @@
+//
+//  aecombinerTests.swift
+//  aecombinerTests
+//
+//  Created by David JM Lewis on 25/06/2015.
+//  Copyright (c) 2015 djml.eu. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class aecombinerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
