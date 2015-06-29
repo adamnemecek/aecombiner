f903f13c4cf47d8444da3f8976a21db1fa13cc79
djmlewis <djmlewis@mac.com>
2015-06-29 15:40:43 +0100

    csv

--- aecombiner/CSVdata.swift
+++ aecombiner/CSVdata.swift
@@ -8,11 +8,56 @@
 
 import Cocoa
 
+let quotationMarks = "\""
+let quotationMarksReplacement = ""
+let commaReplacement = "‚"//,
+let commaDelimiter = ","
+
 class CSVdata {
 
     var headers = [String]()
     var csvData = [[String]]()
     var columnsCount:Int = 0
     
+    class func processCSVfileToData(data: NSData) -> (noErrors:Bool, dataModel:CSVdata)
+    {
+        var dataModel = CSVdata()
+        var processedOK = false
+        var dataAsString = NSString(data: data, encoding: NSUTF8StringEncoding)
+        var arrayOfRowArrays = [[String]]()
+        if dataAsString != nil
+        {
+            dataAsString!.enumerateLinesUsingBlock({ (line, okay) -> Void in
+                //check for "" and replace , inside them
+                if line.rangeOfString(quotationMarks) != nil
+                {
+                    // ‚
+                    var subStrings = line.componentsSeparatedByString(quotationMarks)
+                    // we assume the file is properly formed with "" in pairs
+                    //odd indexed substrings are the substrings between "", even substrings are OUTSIDE the ""
+                    // empty strings used to pad start and end
+                    //replace , with special , inside the ''
+                    for var substringIndex=1; substringIndex < subStrings.count; substringIndex += 2
+                    {
+                        subStrings[substringIndex] = subStrings[substringIndex].stringByReplacingOccurrencesOfString(commaDelimiter, withString: commaReplacement)
+                    }
+                    arrayOfRowArrays.append(quotationMarksReplacement.join(subStrings).componentsSeparatedByString(commaDelimiter))
+                }
+                else
+                {
+                    arrayOfRowArrays.append(line.componentsSeparatedByString(commaDelimiter))
+                }
+            })
+            if arrayOfRowArrays.count > 0
+            {
+                dataModel.headers = arrayOfRowArrays[0]
+                dataModel.columnsCount = arrayOfRowArrays[0].count
+                arrayOfRowArrays.removeAtIndex(0)
+                dataModel.csvData = arrayOfRowArrays
+                processedOK = true
+            }
+        }
+        return (processedOK, dataModel)
+    }
 
 }
--- aecombiner/Document.swift
+++ aecombiner/Document.swift
@@ -7,9 +7,6 @@
 //
 
 import Cocoa
-let quotationMarks = "\""
-let commaReplacement = "‚"//,
-let commaDelimiter = ","
 
 
 
@@ -56,51 +53,16 @@ class Document: NSDocument {
         switch typeName
         {
         case "csvFile":
-            var readOK = self.processCSVfileToData(data)
-            return readOK
+            let processedOK = CSVdata.processCSVfileToData(data)
+            if processedOK.noErrors
+            {
+                self.cvsDataModel = processedOK.dataModel
+            }
+            return processedOK.noErrors
         default:
             return false
         }
         
-    }
-
-    func processCSVfileToData(data: NSData) -> Bool
-    {
-        var dataAsString = NSString(data: data, encoding: NSUTF8StringEncoding)
-        var arrayOfRowArrays = [[String]]()
-        if dataAsString != nil
-        {
-            dataAsString!.enumerateLinesUsingBlock({ (line, okay) -> Void in
-                //check for "" and replace , inside them
-                if line.rangeOfString(quotationMarks) != nil
-                {
-                    // ‚
-                    var subStrings = line.componentsSeparatedByString(quotationMarks)
-                    // we assume the file is properly formed with "" in pairs
-                    //odd indexed substrings are the substrings between "", even substrings are OUTSIDE the ""
-                    // empty strings used to pad start and end
-                    //replace , with special , inside the ''
-                    for var substringIndex=1; substringIndex < subStrings.count; substringIndex += 2
-                    {
-                        subStrings[substringIndex] = subStrings[substringIndex].stringByReplacingOccurrencesOfString(commaDelimiter, withString: commaReplacement)
-                    }
-                    arrayOfRowArrays.append("".join(subStrings).componentsSeparatedByString(commaDelimiter))
-                }
-                else
-                {
-                    arrayOfRowArrays.append(line.componentsSeparatedByString(commaDelimiter))
-                }
-            })
-            if arrayOfRowArrays.count > 0
-            {
-                self.cvsDataModel.headers = arrayOfRowArrays[0]
-                self.cvsDataModel.columnsCount = arrayOfRowArrays[0].count
-                arrayOfRowArrays.removeAtIndex(0)
-                self.cvsDataModel.csvData = arrayOfRowArrays
-                return true
-            }
-        }
-        return false
     }
 
 }
