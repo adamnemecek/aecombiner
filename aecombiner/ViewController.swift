c9394304472db968aa1f8b9922d89468f2720136
djmlewis <djmlewis@mac.com>
2015-07-08 12:46:45 +0100

    HH

--- aecombiner/ViewController.swift
+++ aecombiner/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
     
 
     // MARK: - class vars
-
     var parametersArray = [[String]]()
+    var selectedParametersArray = [[String]]()
+    
 
     // MARK: - class constants
     let kParametersTableParametersColumnIndex = 0
@@ -28,6 +29,7 @@ class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelega
 
     @IBOutlet weak var tableViewCSVdata: NSTableView!
     @IBOutlet weak var tableViewHeaders: NSTableView!
+    @IBOutlet weak var tableViewSelectedParameters: NSTableView!
     @IBOutlet weak var tableViewSetOfParameters: NSTableView!
     @IBOutlet weak var textFieldColumnRecodedName: NSTextField!
     
