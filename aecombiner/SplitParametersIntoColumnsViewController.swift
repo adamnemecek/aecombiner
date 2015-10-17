04b2df0a3d82482517751915bbd503ce566eb78d
djmlewis <dl0011@surrey.ac.uk>
2015-10-17 15:04:07 +0100

    pre split

---
+++ aecombiner/SplitParametersIntoColumnsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SplitParametersIntoColumnsViewController.swift
+//  aecombiner
+//
+//  Created by David Lewis on 17/10/2015.
+//  Copyright © 2015 djml.eu. All rights reserved.
+//
+
+import Cocoa
+
+class SplitParametersIntoColumnsViewController: GroupParametersViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
